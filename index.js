270c913db8535036d27aa52900725d725b8d75d0
savidhps <savidhps00@gmail.com>
2025-04-10 23:55:11 +0530

    json-server created

---
+++ index.js
@@ -0,0 +1,24 @@
+// import json-server 
+const jsonserver=require('json-server')
+
+// create server 
+const accountabilityServer=jsonserver.create()
+
+// create a middleware
+
+const middleware=jsonserver.defaults()
+
+// set path to store data 
+const routes=jsonserver.router("db.json")
+
+accountabilityServer.use(middleware)
+accountabilityServer.use(routes)
+
+// server port 
+PORT=4000||process.env.PORT
+
+// server run 
+accountabilityServer.listen(PORT,()=>{
+    console.log(`server running sucessfully ${PORT}`);
+    
+})
